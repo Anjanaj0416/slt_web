d44bcf113dad546bda9cd36fee3ca3b11f810f18
Nabed Khan <nabed420@gmail.com>
2023-09-27 20:48:26 +0600

    disable meta type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import { Open_Sans } from "next/font/google";
 
 export const openSans = Open_Sans({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Bazaar - Next.js E-commerce Template",
   description: `Bazaar is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
   authors: [{ name: "UI-LIB", url: "https://ui-lib.com" }],
