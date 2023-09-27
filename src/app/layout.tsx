60b2035fc23bcfd435e85507ddf863e75d025e73
Nabed Khan <nabed420@gmail.com>
2023-09-27 22:54:20 +0600

    remove meta from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
-import type { Metadata } from "next";
 import React, { ReactNode } from "react";
 import { Open_Sans } from "next/font/google";
 
 export const openSans = Open_Sans({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Bazaar - Next.js E-commerce Template",
-  description: `Bazaar is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
-  authors: [{ name: "UI-LIB", url: "https://ui-lib.com" }],
-  viewport: "width=device-width, initial-scale=1",
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react"],
-};
 
 // THEME PROVIDER
 import ThemeProvider from "theme/theme-provider";
