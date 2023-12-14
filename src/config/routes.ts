58ce9c2835638cca77730e29b33fc21f7e66ca23
Nipuna Akalana <nipuna@surge.global>
2023-12-15 01:09:20 +0530

    Added landing page changes and next auth

---
+++ src/config/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  ADD_STORE: "/vendor/stores/create",
+  EDIT_STORE: "/vendor/stores/[id]/edit",
+  VIEW_STORE: "/vendor/stores/",
+  VIEW_PRODUCT_LISTS: "/vendor/products/",
+};
