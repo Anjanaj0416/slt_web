ceed3fe30f45555af2d40d2803833bea39adabb4
Thakshara Dhananjaya <thakshara@surge.global>
2024-04-01 19:17:09 +0530

    Bugfix: wishlist

--- src/components/product-cards/product-card-10/product-card.tsx
+++ src/components/product-cards/product-card-10/product-card.tsx
@@ -26,6 +26,7 @@ import { Product1 } from "models/Product.model";
 import { useSession } from "next-auth/react";
 import { User1 } from "models/User.model";
 import { useUpdateWishlistMutation } from "services/wishlist-api";
+import { useSnackbar } from "notistack";
 
 // ==============================================================
 type Props = { product: Product1 };
@@ -33,10 +34,10 @@ type Props = { product: Product1 };
 
 const ProductCard20: FC<Props> = ({ product }) => {
   const { id, price, name, images } = product;
-  const [updateWishlist, { isLoading: isUpdating }] =
-    useUpdateWishlistMutation();
+  const [updateWishlist] = useUpdateWishlistMutation();
   const { data, update: updateSession } = useSession();
   const { setIsOpen: openUnAuthenticatedModal } = useUnAuthenticatedModal();
+  const { enqueueSnackbar } = useSnackbar();
 
   const user = data?.user as User1;
   // Map products to product ids
@@ -55,16 +56,28 @@ const ProductCard20: FC<Props> = ({ product }) => {
 
   const toggleFavorite = async () => {
     if (user?.id) {
-      const productIds = wishListProductIds.includes(id)
-        ? wishListProductIds.filter((productId) => productId != id)
-        : [...wishListProductIds, id];
+      const products = wishListProductIds.includes(id)
+        ? user?.wishlist?.products.filter((product) => product.id != id)
+        : [...user?.wishlist?.products, product];
+      //
+      const newWishlist = { ...user?.wishlist, products };
+      updateSession({ ...data, user: { ...user, wishlist: newWishlist } });
+      //
       const response = await updateWishlist({
         userId: user.id,
         wishlistId: user.wishlist.id,
-        body: { productIds },
+        body: { productIds: products.map((product) => product.id) },
       });
-      if (!isUpdating && "data" in response) {
-        updateSession({ ...data, user: { ...user, wishlist: response?.data } });
+      //
+      if (!("data" in response)) {
+        updateSession({ ...data, user: { ...user, wishlist: user?.wishlist } });
+        enqueueSnackbar("Something went to wrong", {
+          variant: "error",
+          anchorOrigin: {
+            vertical: "bottom",
+            horizontal: "right",
+          },
+        });
       }
     } else {
       openUnAuthenticatedModal(true);
