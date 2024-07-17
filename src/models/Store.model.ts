4a0de68c426fbc58f0e7c892562a34f830f620d3
Thakshara Dhananjaya <thakshara@surge.global>
2024-07-18 01:53:21 +0530

    Create Store model

--- src/models/Store.model.ts
+++ src/models/Store.model.ts
@@ -1,5 +1,22 @@
+import Category1 from "./Category.model";
+import SocialLink from "./SocialLink.model";
+
 export default interface Store {
   id: string;
+  socialLinks?: SocialLink[];
   name: string;
+  description: string;
+  category: Category1;
+  telephone?: string;
+  fax?: string;
+  whatsapp?: string;
+  address?: string;
+  email?: string;
+  website?: string;
   logoFilePath: string;
+  storeStatus: StoreStatus;
+  createdAt?: Date;
+  lastUpdatedAt?: Date;
 }
+
+export type StoreStatus = "IN_REVIEW" | "PUBLISHED" | "UNPUBLISHED";
