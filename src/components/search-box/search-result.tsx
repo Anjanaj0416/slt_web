c15d2c1516c04e6f4e5ae6f9d27c753749ef1256
Thakshara Dhananjaya <thakshara@surge.global>
2024-07-25 02:11:15 +0530

    feat: Implement store search

--- src/components/search-box/search-result.tsx
+++ src/components/search-box/search-result.tsx
@@ -3,16 +3,50 @@ import Link from "next/link";
 import MenuItem from "@mui/material/MenuItem";
 // STYLED COMPONENT
 import { SearchResultCard } from "./styled";
+import StoreProductSearch from "../../models/StoreProductSearch.model";
+import { Avatar } from "@mui/material";
+import { ENVIRONMENT } from "config";
 
 // ==============================================================
-type Props = { results: string[] };
+type Props = {
+  results: StoreProductSearch[];
+};
 // ==============================================================
 
 const SearchResult: FC<Props> = ({ results }) => (
   <SearchResultCard elevation={2}>
     {results.map((item) => (
-      <Link href={`/products/search/${item}`} key={item}>
-        <MenuItem key={item}>{item}</MenuItem>
+      <Link
+        href={
+          item.type === "PRODUCT"
+            ? `/products/search/${item.name}`
+            : `/shops/${item.id}`
+        }
+        key={item.id}
+      >
+        {item.type === "STORE" ? (
+          <MenuItem
+            key={item.name}
+            sx={{
+              borderBottom: "1px solid",
+              borderBottomColor: "secondary.100",
+            }}
+          >
+            <Avatar
+              alt={item.name}
+              src={`${ENVIRONMENT.S3_BUCKET_URL}/${item.imageUrl}`}
+              sx={{
+                marginRight: "8px",
+                borderRadius: "10px",
+                height: "auto",
+                width: { sm: 36, xs: "100%" },
+              }}
+            />
+            {item.name}
+          </MenuItem>
+        ) : (
+          <MenuItem key={item.name}>{item.name}</MenuItem>
+        )}
       </Link>
     ))}
   </SearchResultCard>
---
+++ src/models/StoreProductSearch.model.ts
@@ -0,0 +1,6 @@
+export default interface StoreProductSearch {
+  id: string;
+  name: string;
+  imageUrl: string;
+  type: "STORE" | "PRODUCT";
+}
