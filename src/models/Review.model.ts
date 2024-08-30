672f6eca5120a4cfa592fe67178762567eda522a
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2024-08-30 18:04:58 +0530

    Create RTK query to review

--- src/models/Review.model.ts
+++ src/models/Review.model.ts
@@ -1,4 +1,4 @@
-import User from "./User.model";
+import User, { User1 } from "./User.model";
 import Product from "./Product.model";
 
 interface Review {
@@ -8,6 +8,21 @@ interface Review {
   comment: string;
   product: Product;
   published?: boolean;
+}
+
+export interface Review1 {
+  id: string;
+  rating: number;
+  user: User1;
+  text: string;
+  productId: string;
+  createdAt: Date;
+}
+
+export interface POSTReviewRequest {
+  rating: number;
+  text: string;
+  productId: string;
 }
 
 export default Review;
