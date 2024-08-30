12aa318110fd940a6f4e4c66df3fce70fd058f57
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2024-08-30 18:06:09 +0530

    Implement customer reviews

---
+++ src/constants/review.ts
@@ -0,0 +1,12 @@
+/**
+ * Defines the API paths for reviews
+ */
+const API = {
+    GET_REVIEWS: {
+      path: "/reviews?:query",
+      method: "GET",
+    }
+  };
+  //
+  export default API;
+  
