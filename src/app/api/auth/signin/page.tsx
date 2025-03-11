343dcc9d3a952226e0afc47e915cd72359957be3
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-03-11 10:32:40 +0530

    debug: signin

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export default function Page() {
   useEffect(() => {
-    signIn("keycloak", { callbackUrl: "/orders" });
+   // signIn("keycloak", { callbackUrl: "/orders" });
   }, []);
   return <div></div>;
 }
