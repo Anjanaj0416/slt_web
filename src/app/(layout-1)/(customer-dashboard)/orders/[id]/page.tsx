1343906a9bbc75e6949435b23c2a53a87035b93c
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-05-28 00:06:45 +0530

    fix: remove unnecessary metadata fields from order details page

--- src/app/(layout-1)/(customer-dashboard)/orders/[id]/page.tsx
+++ src/app/(layout-1)/(customer-dashboard)/orders/[id]/page.tsx
@@ -8,10 +8,7 @@ import { cachedRequest } from "utils/request";
 
 export const metadata: Metadata = {
   title: "Order Details - TRADEZ ",
-  description: `TRADEZ is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
-  authors: [{ name: "UI-LIB", url: "https://ui-lib.com" }],
   viewport: "width=device-width, initial-scale=1",
-  keywords: ["e-commerce", "e-commerce template", "next.js", "react"],
 };
 
 export default async function OrderDetails({
