44c7979a9fd36ca43185e6b4ab2dc9340e922116
Thakshara Dhananjaya <thaksharadhananjaya@gmail.com>
2025-08-26 12:02:21 +0530

    fix: responsive issues

--- src/pages-sections/market-2/mobile-banner-card.tsx
+++ src/pages-sections/market-2/mobile-banner-card.tsx
@@ -5,12 +5,15 @@ import styled from "@mui/material/styles/styled";
 // STYLED COMPONENTS
 const CardWrapper = styled(Box, {
   shouldForwardProp: (prop) => prop !== "img", // prevent img from being passed to DOM
-})<{ img: string }>(({ img }) => ({
+})<{ img: string }>(({theme, img }) => ({
 
   cursor: "pointer",
   height: 200,
   maxHeight: 240,
   width: "45vw",
+  [theme.breakpoints.down(400)]: {
+    width: "43vw",
+  },
   borderRadius: 8,
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
